673080203b119d7a95de3f7c01ae3af9677fb594
Adeline L <adeline.labesse55@gmail.com>
2025-12-12 16:43:32 +0100

    fix: add missing fields in bet model

--- services/bets/api/src/models/bet.ts
+++ services/bets/api/src/models/bet.ts
@@ -3,6 +3,8 @@ import {IModelBase} from "./modelBase";
 export interface Bet extends IModelBase {
     user_id: number;
     match_id: number;
+    team_id: number;
+    bet_type: string;
     cote: number;
     bet_amount: number;
     status: string;
--- services/bets/db/init.sql
+++ services/bets/db/init.sql
@@ -5,6 +5,8 @@ CREATE TABLE IF NOT EXISTS bets (
                                       id INT AUTO_INCREMENT PRIMARY KEY,
                                       user_id INT NOT NULL,
                                       match_id INT NOT NULL,
+                                      team_id INT NOT NULL,
+                                      bet_type ENUM('victory', 'defeat', 'draw') DEFAULT 'victory',
                                       cote FLOAT NOT NULL,
                                       bet_ammount FLOAT NOT NULL,
                                       status ENUM('in_progress', 'lost', 'won', 'cancelled') DEFAULT 'in_progress',
@@ -13,7 +15,7 @@ CREATE TABLE IF NOT EXISTS bets (
     );
 
 
--- Insert sample matches
-INSERT INTO bets (user_id, match_id, cote, bet_ammount, status) VALUES
-                                                                                         (1, 1, 2, 52, 'cancelled'),
-                                                                                         (1, 3, 2, 126, 'won');
+-- Insert sample bets
+INSERT INTO bets (user_id, match_id, team_id, bet_type, cote, bet_ammount, status) VALUES
+                                                                                         (1, 1, 2, 'draw', 2, 52, 'cancelled'),
+                                                                                         (1, 3, 2, 'victory', 8, 126, 'won');
